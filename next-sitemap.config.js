d295c7d100b7afec72563805cd9aaca75bbeb217
WEI JUN <weijunext@gmail.com>
2024-03-15 11:01:23 +0800

    chore: env SITE_URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.URL || "https://nextjstranslate.com",
+  siteUrl: process.env.SITE_URL || "https://nextjstranslate.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
