fd215bfcc58fdec552feb69bab191460390449ee
WEI JUN <weijunext@gmail.com>
2024-03-19 17:10:04 +0800

    style: Modify the order of products in the footer

--- config/site.ts
+++ config/site.ts
@@ -36,10 +36,11 @@ const baseSiteConfig = {
   ],
   footerProducts: [
     { url: 'https://weijunext.com/', name: 'J实验室' },
-    { url: 'https://githubbio.com', name: 'Github Bio Generator' },
     { url: 'https://smartexcel.cc/', name: 'Smart Excel' },
-    { url: 'https://starter.weijunext.com/', name: 'Next.js Starter' },
+    { url: 'https://landingpage.weijunext.com/', name: 'Landing Page Boilerplate' },
     { url: 'https://nextjs.weijunext.com/', name: 'Next.js Practice' },
+    { url: 'https://starter.weijunext.com/', name: 'Next.js Starter' },
+    { url: 'https://githubbio.com', name: 'Github Bio Generator' },
     { url: 'https://github.com/weijunext/indie-hacker-tools', name: 'Indie Hacker Tools' },
     { url: SITE_URL, name: 'Next.js 中文文档' },
   ]
