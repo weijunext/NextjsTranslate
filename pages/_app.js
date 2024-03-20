e6d968db120167300c073e7b1225cc82335bbbb0
WEI JUN <weijunext@gmail.com>
2024-03-20 14:10:21 +0800

    feat: 修改目录

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
-import Head from 'next/head'
-
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  
   return (
     <>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
